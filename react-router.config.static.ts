cd5c5378434acec6cfbb2057529cc2cc94b9eb41
Bruno-116aff <admt005@gmail.com>
2025-10-03 18:49:21 +0300

    Исправление конфигурации для статического экспорта

--- react-router.config.static.ts
+++ react-router.config.static.ts
@@ -1,9 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // Отключаем SSR для статического экспорта
+  ssr: false,
+  
+  // Включаем предварительный рендеринг всех маршрутов
+  prerender: {
+    routes: ['/']
+  },
+  
   // Настройки для GitHub Pages
   future: {
     v3_fetcherPersist: true,
